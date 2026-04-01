7ca7f406b22bf56afbfb48b179e501d4b54e2592
JosharoozOS <heinjos@oregonstate.edu>
2026-04-01 09:45:04 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Josh Hein
+# GitHub username: JosharoozOS
+# Date: 4/1/2026
+# Description: Asks the user for their favorite animal and then
+#              prints out their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
